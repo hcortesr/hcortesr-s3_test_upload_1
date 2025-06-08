3f6836082b09142f98c1b442b168d16cf9370a67
hcortesr <hectorcortesr04@gmail.com>
2025-06-08 11:29:51 -0600

    Se esta configurando el molten, pero me tuve que descargar POSTMAN

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { config } from 'dotenv';
+import multer, { MulterError } from "multer";
+
+config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('¡Hola desde Express con TypeScript!');
+const upload = multer({ dest: "uploads/" });
+
+app.post('/file', upload.single('my_file'), (req, res) => {
+
+    console.log(req.file)
+    console.log("asf");
+    
+    
+    res.send('¡Hola desde Express con TypeScript!');
 });
 
+
+
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
